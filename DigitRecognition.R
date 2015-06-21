5bf12523240986540892fe85b47341b55370f1da
Raghav Nayak <rnayak@hotmail.com>
2015-06-21 19:44:49 +0530

    R Script for Kaggle MNIST Digit recognizer

---
+++ DigitRecognition.R
@@ -0,0 +1,49 @@
+#MNIST Digit recognition using SVM & PCA
+#Author Raghav Nayak M
+
+library(e1071)
+library(caret)
+
+#Read training and test dataset
+train <- read.csv("train.csv", header=TRUE)
+test <- read.csv("test.csv", header=TRUE)
+labels <- train[,1]
+train <- train[,-1]
+
+#Normalize data
+norm = preProcess(train)
+train = predict(norm, train)
+test = predict(norm, test)
+
+#Start applying PCA for dimentionality reduction
+nw <- cov(train);
+
+nd <- svd(nw);
+
+u <- nd$u[,1:120];
+
+train_names <- colnames(train);
+train_names <- train_names[1:120];
+
+train <- as.matrix(train);
+
+train <- train %*% u
+
+colnames(train) <- train_names;
+
+dataset =data.frame(label=as.factor(labels), train);
+
+#Train SVM with polynomial kernel of degree 9
+model = svm(label ~., data = dataset, kernel = "polynomial", degree = 2)
+
+test <- as.matrix(test);
+
+test <- test %*% u;
+
+#Column names for training dataset and testing would be same :)
+colnames(test) <- train_names;
+
+#Prepare output file
+pred = predict(model, newdata = test)
+predictions = data.frame(ImageId=1:nrow(test), Label=pred)
+write.csv(predictions,"predictions-new.csv", row.names= FALSE)
